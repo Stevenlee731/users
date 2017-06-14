6735b4386cf34de5abcb568daff127c7cc57e723
stevecreated@gmail.com <stevecreated@gmail.com>
2017-06-14 15:04:13 -0700

    populate blogpost in association test

--- test/association_test.js
+++ test/association_test.js
@@ -1,20 +1,16 @@
 const mongoose = require('mongoose')
+const assert = require('assert')
 const User = require('../src/user')
 const Comment = require('../src/comment')
 const BlogPost = require('../src/blogPost')
 
-describe('Associations', () => {
+describe('Assocations', () => {
   let joe, blogPost, comment
 
   beforeEach(done => {
-    joe = new User({ name: 'Joe'})
-    blogPost = new BlogPost({
-      title: 'JS is Great',
-      content: 'Yep it is'
-    })
-    comment = new Comment({
-      content: 'Congrats on great post'
-    })
+    joe = new User({ name: 'Joe' })
+    blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is' })
+    comment = new Comment({ content: 'Congrats on great post' })
 
     joe.blogPosts.push(blogPost)
     blogPost.comments.push(comment)
@@ -24,12 +20,12 @@ describe('Associations', () => {
       .then(() => done())
   })
 
-  it.only('saves a relation between a user and a blogpost', done => {
+  it('saves a relation between a user and a blogpost', done => {
     User.findOne({ name: 'Joe' })
-      .then(user => {
-        console.log(user)
+      .populate('blogPosts')
+      .then((user) => {
+        assert(user.blogPosts[0].title === 'JS is Great')
         done()
       })
-
   })
 })
